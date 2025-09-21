d0f05bdd487d0388cc722335596b15891e31182c
Roi Bachynskyi <roybachynskyi@gmail.com>
2025-09-21 20:51:05 +0300

    Not working example

--- src/bin/pwm.rs
+++ src/bin/pwm.rs
@@ -8,8 +8,8 @@ use nxp_pac::*;
 use {defmt_rtt as _, panic_halt as _};
 
 // const DIVIDER: u8 = 96;
-const DUTY_CYCLE: u32 = 20;
-const TOP: u32 = u32::max_value();
+const DUTY_CYCLE: u32 = 50;
+const TOP: u32 = 0xffff;
 const OUTPUT_PIN: usize = 1;
 
 fn init() {
@@ -54,21 +54,19 @@ fn init() {
         w.set_autolimit_l(true);
     });
 
-    
-
     SCT0.match0().modify(|w| {
         w.set_matchn_l((TOP & 0xFFFF) as u16);
         w.set_matchn_h((TOP >> 16) as u16);
     });
 
-    SCT0.match2().modify(|w| {
-        w.set_matchn_l(0);
-        w.set_matchn_h(0);
-    });
+    // SCT0.match2().modify(|w| {
+    //     w.set_matchn_l(0);
+    //     w.set_matchn_h(0);
+    // });
 
     // SCT0.matchrel0().modify(|w| {
-    // w.set_reloadn_l((TOP & 0xFFFF) as u16);
-    // w.set_reloadn_h((TOP >> 16) as u16);
+    //     w.set_reloadn_l(0);
+    //     w.set_reloadn_h(0);
     // });
 
     // SCT0.matchrel0().modify(|w| {
@@ -96,13 +94,11 @@ fn init() {
     });
 
     // SCT0.matchrel2().modify(|w| {
-    //     w.set_reloadn_l(0);
+    //     w.set_reloadn_l(1);
     //     w.set_reloadn_h(0);
     // });
 
     // Events
-
-    
 
     SCT0.ev(0).ev_ctrl().modify(|w| {
         w.set_matchsel(2);
@@ -117,14 +113,23 @@ fn init() {
         w.set_statev(0);
     });
 
-    SCT0.start().modify(|w| w.set_startmsk_l(1 << 0));
-
-    SCT0.halt().modify(|w| {
-        w.set_haltmsk_l(0);
+    // You MUST assign MATCHO to any event
+    SCT0.ev(2).ev_ctrl().modify(|w| {
+        w.set_matchsel(0);
+        w.set_combmode(sct0::vals::Combmode::MATCH);
+        w.set_stateld(sct0::vals::Stateld::ADD);
+        w.set_statev(0);
     });
+
+    // SCT0.start().modify(|w| w.set_startmsk_l(1 << 0));
+
+    // SCT0.halt().modify(|w| {
+    //     w.set_haltmsk_l(0);
+    // });
 
     SCT0.ev(0).ev_state().modify(|w| w.set_statemskn(1 << 0));
     SCT0.ev(1).ev_state().modify(|w| w.set_statemskn(1 << 0));
+    SCT0.ev(2).ev_state().modify(|w| w.set_statemskn(1 << 0));
 
     SCT0.out(OUTPUT_PIN).out_set().modify(|w| w.set_set(1 << 0));
     SCT0.out(OUTPUT_PIN).out_clr().modify(|w| w.set_clr(1 << 1));
@@ -134,6 +139,7 @@ fn init() {
     SCT0.ctrl().modify(|w| {
         w.set_clrctr_l(true);
         w.set_halt_l(false);
+        w.set_pre_h(96 - 1);
     });
 }
 
@@ -142,10 +148,24 @@ async fn main(_spawner: Spawner) {
     init();
 
     info!("Inside the main function");
+
+    info!("Counter: {:b}", SCT0.count().read().0);
+    for _ in 0..100_000 {
+        nop();
+    }
+    info!("Lower bits {:b}", SCT0.count().read().ctr_l());
+    info!("Upper bits {:b}", SCT0.count().read().ctr_h());
+    info!("Is unified? {}", SCT0.config().read().unify().to_bits());
+    info!("Is counting down? {}", SCT0.ctrl().read().down_l());
+    info!("Is halted? {} ", SCT0.ctrl().read().halt_l());
+    info!(
+        "Is bidirectional? {}",
+        SCT0.ctrl().read().bidir_l().to_bits()
+    );
+    info!("Is stopped? {}", SCT0.ctrl().read().stop_l());
     loop {
-        info!("Lower bits {:b}", SCT0.count().read().ctr_l());
-        info!("Upper bits {:b}", SCT0.count().read().ctr_h());
-        for _ in 0..100_000{
+        info!("Counter: {:b}", SCT0.count().read().0);
+        for _ in 0..100_000 {
             nop();
         }
     }

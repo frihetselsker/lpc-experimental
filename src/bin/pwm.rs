690d32721c64ebc8f223aafc86334a3f4e5bea2f
Roi Bachynskyi <roybachynskyi@gmail.com>
2025-09-22 00:08:56 +0300

    PWM working Output example

--- src/bin/pwm.rs
+++ src/bin/pwm.rs
@@ -7,9 +7,8 @@ use embassy_executor::Spawner;
 use nxp_pac::*;
 use {defmt_rtt as _, panic_halt as _};
 
-// const DIVIDER: u8 = 96;
-const DUTY_CYCLE: u32 = 20;
-const TOP: u32 = 0xffff;
+const DUTY_CYCLE: u32 = 1;
+const TOP: u32 = 200_000;
 const OUTPUT_PIN: usize = 1;
 
 fn init() {
@@ -36,9 +35,12 @@ fn init() {
         .sctclksel()
         .modify(|w| w.set_sel(syscon::vals::SctclkselSel::ENUM_0X3));
 
+    // Divide clock by 1-256
+
     // SYSCON.sctclkdiv().modify(|w| w.set_div(DIVIDER - 1));
 
     // Reset SCTimer
+
     info!("Reset SC Timer");
     SYSCON
         .presetctrl1()
@@ -48,29 +50,37 @@ fn init() {
         .modify(|w| w.set_sct_rst(syscon::vals::SctRst::RELEASED));
 
     // Configure SCTimer
+
     SCT0.config().modify(|w| {
-        w.set_unify(sct0::vals::Unify::UNIFIED_COUNTER);
+        w.set_unify(sct0::vals::Unify::UNIFIED_COUNTER); // 32-bit counter for now, can be configured so that it has two separate counters
         w.set_clkmode(sct0::vals::Clkmode::SYSTEM_CLOCK_MODE);
         w.set_autolimit_l(true);
     });
 
+    // Match 0 will reset the timer using TOP value
+
     SCT0.match0().modify(|w| {
-        w.set_matchn_l(((4 * TOP) & 0xFFFF) as u16);
-        w.set_matchn_h(((4 * TOP) >> 16) as u16);
+        w.set_matchn_l((TOP & 0xFFFF) as u16);
+        w.set_matchn_h((TOP >> 16) as u16);
     });
+
+    // It is OBLIGATORY to write the reload value
+    // because after each limit match values are cleared
 
     SCT0.matchrel0().modify(|w| {
-        w.set_reloadn_l(((4 * TOP) & 0xFFFF) as u16);
-        w.set_reloadn_h(((4 * TOP) >> 16) as u16);
+        w.set_reloadn_l((TOP & 0xFFFF) as u16);
+        w.set_reloadn_h((TOP >> 16) as u16);
     });
 
-    // The actual matches
+    // The actual matches that are used for event logic
 
     SCT0.match1().modify(|w| {
         let value: u32 = (TOP / 100) * DUTY_CYCLE;
         w.set_matchn_l((value & 0xFFFF) as u16);
         w.set_matchn_h((value >> 16) as u16);
     });
+
+    // Reload value is OBLIGATORY
 
     SCT0.matchrel1().modify(|w| {
         let value: u32 = (TOP / 100) * DUTY_CYCLE;
@@ -83,78 +93,43 @@ fn init() {
         w.set_matchn_h(0);
     });
 
+    // Reload value is OBLIGATORY
+
     SCT0.matchrel2().modify(|w| {
         w.set_reloadn_l(0);
         w.set_reloadn_h(0);
     });
 
-    SCT0.match3().modify(|w| {
-        w.set_matchn_l(((2 * TOP) & 0xFFFF) as u16);
-        w.set_matchn_h(((2 * TOP) >> 16) as u16);
-    });
-
-    SCT0.matchrel3().modify(|w| {
-        w.set_reloadn_l(((2 * TOP) & 0xFFFF) as u16);
-        w.set_reloadn_h(((2 * TOP) >> 16) as u16);
-    });
-
-    SCT0.match4().modify(|w| {
-        w.set_matchn_l(((3 * TOP) & 0xFFFF) as u16);
-        w.set_matchn_h(((3 * TOP) >> 16) as u16);
-    });
-
-    SCT0.matchrel4().modify(|w| {
-        w.set_reloadn_l(((3 * TOP) & 0xFFFF) as u16);
-        w.set_reloadn_h(((3 * TOP) >> 16) as u16);
-    }); // Events
+    // Event configuration
 
     SCT0.ev(0).ev_ctrl().modify(|w| {
         w.set_matchsel(2);
         w.set_combmode(sct0::vals::Combmode::MATCH);
-        w.set_stateld(sct0::vals::Stateld::ADD);
+        w.set_stateld(sct0::vals::Stateld::ADD); // STATE + statev
         w.set_statev(0);
     });
     SCT0.ev(1).ev_ctrl().modify(|w| {
         w.set_matchsel(1);
         w.set_combmode(sct0::vals::Combmode::MATCH);
-        w.set_stateld(sct0::vals::Stateld::ADD);
+        w.set_stateld(sct0::vals::Stateld::ADD); // STATE + statev
         w.set_statev(0);
     });
 
-    SCT0.ev(2).ev_ctrl().modify(|w| {
-        w.set_matchsel(3);
-        w.set_combmode(sct0::vals::Combmode::MATCH);
-        w.set_stateld(sct0::vals::Stateld::ADD);
-        w.set_statev(0);
-    });
+    // Statev can move to another state
 
-    SCT0.ev(3).ev_ctrl().modify(|w| {
-        w.set_matchsel(4);
-        w.set_combmode(sct0::vals::Combmode::MATCH);
-        w.set_stateld(sct0::vals::Stateld::ADD);
-        w.set_statev(0);
-    });
-
-    SCT0.start().modify(|w| w.set_startmsk_l(0));
-
-    SCT0.halt().modify(|w| {
-        w.set_haltmsk_l(0);
-    });
-
+    // Assign events to states
     SCT0.ev(0).ev_state().modify(|w| w.set_statemskn(1 << 0));
     SCT0.ev(1).ev_state().modify(|w| w.set_statemskn(1 << 0));
-    // SCT0.ev(2).ev_state().modify(|w| w.set_statemskn(1 << 0));
-    // SCT0.limit().modify(|w| w.set_limmsk_l(1 << 2));
-    SCT0.out(OUTPUT_PIN).out_set().modify(|w| w.set_set(1 << 0));
-    SCT0.out(OUTPUT_PIN).out_clr().modify(|w| w.set_clr(1 << 1));
-    SCT0.out(OUTPUT_PIN).out_clr().modify(|w| w.set_clr(1 << 2));
-    SCT0.out(OUTPUT_PIN).out_set().modify(|w| w.set_set(1 << 3));
-    SCT0.state().modify(|w| w.set_state_l(0));
+
+    SCT0.out(OUTPUT_PIN).out_set().modify(|w| w.set_set(1 << 0)); // High when event 0 is active
+    SCT0.out(OUTPUT_PIN).out_clr().modify(|w| w.set_clr(1 << 1)); // Low when event 1 is active
+
+    SCT0.state().modify(|w| w.set_state_l(0)); // State 0 by default
 
     SCT0.ctrl().modify(|w| {
-        w.set_clrctr_l(true);
-        w.set_halt_l(false);
-        // w.set_pre_l(96 - 1);
+        w.set_clrctr_l(true); // Clear counter before start
+        w.set_halt_l(false); // Remove halt and start the actual counter
+                             // w.set_pre_l(96 - 1); // Division by prescaled value
     });
 }
 
